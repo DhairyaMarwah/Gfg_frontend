4117f33f4d10476dfc29e243d4dae54ea10a68ed
dhairyamarwah <dhairyamarwah01@gmail.com>
2023-04-16 14:50:38 +0530

    update

--- client/src/pages/Demo2.js
+++ client/src/pages/Demo2.js
@@ -1,95 +1,59 @@
-import React, { useState, useEffect } from 'react';
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
+import React, { useRef, useState, useEffect } from "react";
+ 
 function App() {
-  const [transcription, setTranscription] = useState('');
   const [isRecording, setIsRecording] = useState(false);
-  const [chunks, setChunks] = useState([]);
+  const [audioBlob, setAudioBlob] = useState(null);
+  const mediaRecorderRef = useRef(null);
+  const audioRef = useRef(null);
+  const handleStartRecording = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        mediaRecorderRef.current = new MediaRecorder(stream);
+        mediaRecorderRef.current.start();
 
-  const handleStart = () => {
-    setIsRecording(true);
-    setTranscription('');
-    setChunks([]);
-    recognition.start();
+        setIsRecording(true);
+
+        mediaRecorderRef.current.addEventListener("dataavailable", (event) => {
+          setAudioBlob(event.data);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
-  const handleStop = () => {
+  const handleStopRecording = () => {
+    mediaRecorderRef.current.stop();
     setIsRecording(false);
-    recognition.stop();
   };
 
-const handleTranscribe = () => {
-  const apiUrl = 'https://36a2-2401-4900-1c23-45a0-c965-6c45-94e0-3d68.ngrok-free.app/transcribe';
-
-  // Create a new Blob object from the chunks array
-  const audioBlob = new Blob(chunks, { type: 'audio/webm' });
-  console.log(URL.createObjectURL(audioBlob));
-
-  // Create a new FormData object
-  const formData = new FormData();
-
-  // Append the recorded audio as a file to the FormData object
-  formData.append('audio', audioBlob, 'recording.webm');
-
-  // Send the FormData object to the API endpoint
-  fetch(apiUrl, {
-    method: 'POST',
-    body: formData
-  })
-    .then(response => response.json())
-    .then(json => {
-      const transcription = json.transcription;
-      setTranscription(transcription);
-    })
-    .catch(error => console.error(error));
-};
-
-
-
-  useEffect(() => {
-    recognition.interimResults = true;
-    recognition.lang = 'en-US';
-
-    recognition.onresult = (event) => {
-      let interimTranscription = '';
-      let finalTranscription = '';
-
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) {
-          finalTranscription += transcript + ' ';
-        } else {
-          interimTranscription += transcript;
-        }
-      }
-
-      setTranscription(finalTranscription);
-      setChunks(prevChunks => [...prevChunks, ...event.results]);
-    };
-
-    recognition.onerror = (event) => {
-      console.error(event.error);
-    };
-
-    return () => {
-      recognition.abort();
-    };
-  }, []);
+  const handleSubmit = () => {
+    const formData = new FormData();
+    formData.append("audio", audioBlob);
+    console.log(audioBlob);
+    const bloburl = URL.createObjectURL(audioBlob);
+    console.log(bloburl);
+    audioRef.current.src = bloburl; // set the src attribute of the audio element
+  };
 
   return (
-    <div>
-      <h1>Speech-to-Text Demo</h1>
-      {isRecording ? (
-        <button onClick={handleStop}>Stop Recording</button>
-      ) : (
-        <button onClick={handleStart}>Start Recording</button>
-      )}
-      <button onClick={handleTranscribe}>Transcribe</button>
-      <p>{transcription}</p>
-    </div>
+    <>
+      <div>
+        <button onClick={handleStartRecording} disabled={isRecording}>
+          Start Recording
+        </button>
+        <button onClick={handleStopRecording} disabled={!isRecording}>
+          Stop Recording
+        </button>
+        <button onClick={handleSubmit} disabled={!audioBlob}>
+          Submit
+        </button>
+        <audio ref={audioRef} controls /> 
+      </div>
+    </>
   );
 }
+
 
 export default App;
