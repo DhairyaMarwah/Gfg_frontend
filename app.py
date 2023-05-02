41c7e9b46522db94f7d9c52c22aa08c48c61b97a
jayantkhanna1 <jayantkhanna3105@gmail.com>
2023-05-02 15:22:43 +0530

    attached chatpt

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import flask
 from flask import request
 from flask_cors import CORS
 import whisper
+import openai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from transformers import pipeline
 
 app = flask.Flask(__name__)
@@ -13,13 +17,26 @@ CORS(app)
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     if request.method == 'POST':
+        print("transcribing")
         temp_dir = tempfile.mkdtemp()
         save_path = os.path.join(temp_dir, 'temp.wav')
-        wav_file = request.files['audio_data']
+        wav_file = request.files['audio']
         wav_file.save(save_path)
-        print("transcribing")
-        pipe = pipeline(model="jayantkhannna1/whisper-small-en-medical-2.0.3",use_auth_token="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")
+        pipe = pipeline(model=os.environ["MODEL"],use_auth_token=os.environ["HUGGINGFACE_API_KEY"])
         result = pipe(save_path)["text"]
-        return result
+        # get corrected text from openai
+        print(result)
+        question = "Correct the following text and correct any words that may be wrong: \n" + result +" \n\nCorrected text:"
+        openai.api_key = "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"
+        response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo',
+                messages=[
+                    {"role": "user", "content": question},
+                ],
+                n=1
+        )
+        print(response.choices[0]['message']["content"])
+        answer = response.choices[0]['message']["content"]
+        return answer
     else:
         return "This endpoint only processes POST wav blob"
